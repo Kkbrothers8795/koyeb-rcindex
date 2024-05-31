b515832e1a29117e16bf7b390ab7b7b266f2d145
Kamlesh Saini <150409757+Kkbrothers8795@users.noreply.github.com>
2024-05-31 19:22:07 +0530

    Update serve.sh

--- serve.sh
+++ serve.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-wget -qO rclone.conf $CONFIG_URL
+wget -qO rclone.conf https://gist.githubusercontent.com/Kkbrothers8795/a66614bf1ebfb54617eac16346e115cf/raw/d562b430a047c3a14e212eddbe36d364607e4024/rclone
 
 triker listremotes --config rclone.conf \
     | while read line ; do echo "${line%:}"=$line ; done \
@@ -10,7 +10,7 @@ if [ -n "${USERNAME}" ] && [ -n "${PASSWORD}" ]; then
 
 triker serve http combine: \
     --config=rclone.conf \
-    --addr=:$PORT \
+    --addr=:2029 \
     --user=$USERNAME \
     --pass=$PASSWORD \
     --buffer-size=64M \
@@ -22,7 +22,7 @@ else
 
 triker serve http combine: \
     --config=rclone.conf \
-    --addr=:$PORT \
+    --addr=:2029 \
     --buffer-size=64M \
     --template=dark.html \
     --vfs-cache-mode=full \
